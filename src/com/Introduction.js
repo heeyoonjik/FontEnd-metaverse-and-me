206eca2df14c1f20f0ac4a0130394c40e78212bb
heeyoon <heeyoon1302@naver.com>
2022-08-24 00:49:25 +0900

    speed fix

--- src/com/Introduction.js
+++ src/com/Introduction.js
@@ -6,7 +6,7 @@ import "aos/dist/aos.css";
 
 const Introduction = () => {
   useEffect(() => {
-    AOS.init({ duration: 1000 });
+    AOS.init({ duration: 2000 });
   }, []);
   return (
     <Inner>
